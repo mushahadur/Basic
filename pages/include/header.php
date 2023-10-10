7cf2c0f542aeb7fc76ecea2aec6cf703166e9d09
mushahadur <mushahadurrahamankhanpulock@gmail.com>
2023-10-10 14:28:04 +0600

    basic

---
+++ pages/include/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>HOME</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap.css"/>
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Logo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse ml-0" id="navbarTogglerDemo02">
+            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                <li class="nav-item "><a class="nav-link" href="#">Home </a></li>
+                <li class="nav-item "><a class="nav-link" href="#">About </a></li>
+                <li class="nav-item "><a class="nav-link" href="#">Contact </a></li>
+                <li class="nav-item "><a class="nav-link" href="#">link </a></li>
+            </ul>
+            <form class="form-inline ml-5 my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
